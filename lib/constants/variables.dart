8aedbab9e0a5d97973c46dc05f32319e4f5a32bf
MockAirplane700 <sizibamthandazo@yahoo.com>
2023-02-13 19:54:31 -0600

    ready for production

--- lib/constants/variables.dart
+++ lib/constants/variables.dart
@@ -9,7 +9,7 @@ const Color cardBackgroundColor = Colors.white;
 const Color shadowColor = Colors.black;
 // share link to couponsareus
 const String shareUrl = 'https://www.sizibamthandazo.dev/projects/couponsareus';
-const String logoUrl = 'https://firebasestorage.googleapis.com/v0/b/coupons-are-us.appspot.com/o/couponsareus%20logo%202.png?alt=media&token=3a04512f-5f8d-4786-a1c7-c156a0dd2448';
+const String logoUrl = 'https://firebasestorage.googleapis.com/v0/b/coupons-are-us.appspot.com/o/majorKill%2FMy%20project%20(8).png?alt=media&token=3e1b7930-c428-4a73-9c93-836a69748812';
 
 // the color of the drawer header
 const Color drawerHeaderColor = Colors.white30;
